3f7299dd12b24fb61d592ffeda5659a97d0dafe4
Laura Guerrero <yonesky.guer@gmail.com>
2020-11-16 15:37:34 -0400

    Agregando pycopg2.

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@ from db import db
 
 
 class UserModel(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
